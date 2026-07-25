b5dcc63d7998ab3648c93d78424dfc75cb0f27f5
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-07-25 04:18:41 +0300

    تحسين تحميل وحدات العقد المؤسسي v220

--- provider-assessment-demo/institutional-contract-v220.js
+++ provider-assessment-demo/institutional-contract-v220.js
@@ -1,2 +1,4 @@
 "use strict";
-import "./institutional-contract-v220-integration.js";
+import("./institutional-contract-v220-integration.js").catch((error) => {
+  console.error("تعذر تحميل عقد التقييم المؤسسي v220", error);
+});
